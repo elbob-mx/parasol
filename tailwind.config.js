bd5c94f114ceebecbc77f69de85f02f045f9c81b
Roberto Gutierrez <rob7th@gmail.com>
2024-07-01 17:30:09 -0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+        screens: {
+            small: "0px",
+            mobile: "375px",
+            mobileXL: "390px",
+            mobileHorSmall: "567px",
+            mobileHor: "640px",
+            tablet: "767px",
+            bigHor: "811px",
+            bigPhoneHor: "844px",
+            vertIpadMini: "820px",
+            laptop: "976px",
+            ipad: "1023px",
+            hd: "1365px",
+            fhd: "1440px",
+        },
+        extend: {
+            fontFamily: {
+                Manrope: "Manrope, sans-serif",
+                DMserif: "DM Serif Display, serif",
+                IBMserif: "IBM Plex Serif, serif",
+            },
+            colors: {
+                offBlack: "#181818",
+                offWhite: "#F2F2F2",
+                darkParasol: "#001212",
+                midParasol: "#2B5D68",
+                lightParasol: "#7DBBB8",
+                lighterParasol: "#C5D7D9",
+                darkBeige: "#DCD8CF",
+                vividBeige: "#EDDAD6",
+                greige: "#EDECE7",
+            },
+        },
+    },
+    plugins: [],
+};
